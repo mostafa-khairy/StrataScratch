7b5b06204cb00e40ac2844f6e9016a8482722567
Mostafa Khairy Abo Dahab <87584678+mostafa-khairy@users.noreply.github.com>
2022-05-11 04:40:22 +0200

    Create Find the total number of available beds per hosts' nationality.sql

---
+++ Find the total number of available beds per hosts' nationality.sql
@@ -0,0 +1,14 @@
+/*
+
+https://platform.stratascratch.com/coding/10187-find-the-total-number-of-available-beds-per-hosts-nationality?code_type=1
+
+*/
+
+
+
+select sum(n_beds) beds , nationality
+from airbnb_apartments a
+join airbnb_hosts h
+on a.host_id = h.host_id
+group by 2
+order by 1 desc ;
