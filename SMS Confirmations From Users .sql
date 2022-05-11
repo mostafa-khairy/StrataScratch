1d5192c266f5be33099f839f51d3b63045d481fa
Mostafa Khairy Abo Dahab <87584678+mostafa-khairy@users.noreply.github.com>
2022-05-11 04:22:35 +0200

    Create SMS Confirmations From Users .sql

---
+++ SMS Confirmations From Users .sql
@@ -0,0 +1,15 @@
+/*
+
+https://platform.stratascratch.com/coding/10291-sms-confirmations-from-users?code_type=1
+
+*/
+
+
+
+select (count(c.phone_number)  / count(s.phone_number)) *100 as percentage
+from fb_sms_sends s
+left join fb_confirmers c
+on c.phone_number = s.phone_number and c.date = s.ds
+where  s.ds = '08-04-2020' and s.type = 'message' ;
+
+
